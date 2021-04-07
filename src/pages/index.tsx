3fc49212f251c31425d53b6176652207f537103a
Adoilson Freitas <adoilsonfreitas@gmail.com>
2021-04-07 20:57:24 -0300

    creating Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-import style from '../../public/home.module.scss';
+import Head from 'next/head';
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
-    <h1 className={style.title}>Hello Ignews</h1>
+    <>
+    <Head>
+      <title>Home | ig.news</title>
+    </Head>
+   
+    
+    <main className={styles.contentContainer}>
+      <section className={styles.hero}>
+        <span>👏Hey, welcome</span>
+        <h1>News about the <span>React</span> word.</h1>
+        <p>
+          Get access to all the publications <br/>
+          <span>for $9.90 month</span>
+        </p>
+      </section>
+
+      <img src="/images/avatar.svg" alt="Girl coding"/>
+    </main>
+    </>
   )
 }
