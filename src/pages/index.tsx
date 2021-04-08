3549607591af8e535907ff6201093851f1bf8685
Adoilson Freitas <adoilsonfreitas@gmail.com>
2021-04-08 00:46:52 -0300

    SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 
 import Head from 'next/head';
 
@@ -41,10 +41,8 @@ export default function Home({product}: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe.prices.retrieve('price_1Idm8GCVoLylDIa6aQcjy58B', {
-    expand: ['product']
-  });
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1Idm8GCVoLylDIa6aQcjy58B');
 
   const product = {
     priceId: price.id,
@@ -57,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
