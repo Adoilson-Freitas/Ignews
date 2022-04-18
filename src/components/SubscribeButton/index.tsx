43cdc61a46c593000507fe18336771518aa4b71e
Adoilson Freitas <adoilsonfreitas@gmail.com>
2022-04-17 23:51:02 -0300

    Creating Checkout Session in Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,16 +1,29 @@
-import styles from './styles.module.scss';
+import { signin, useSession } from "next-auth/client";
+import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
-  priceId: string
+  priceId: string;
 }
 
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
-  return  (
-    <button 
-    type="button"
-    className={styles.subscribeButton}
-  >
-    Subscribe now
-  </button>
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signin("github");
+      return;
+    }
+
+    // Criação da checkout session
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
   );
 }
